b2a7350802b26e17a8a39fc7d6e8726fcf55bf5c
wanglibo <cyjxwlb@163.com>
2017-12-24 00:05:43 +0800

    second commit

---
+++ app/src/main/java/com/cyjxwlb163/myweather/MainActivity.java
@@ -0,0 +1,29 @@
+package com.cyjxwlb163.myweather;
+
+/**
+ * Created by Administrator on 2017/10/31.
+ */
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.cyjxwlb163.myweather.R;
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+
+
+    }
+
+}
